f22f9a3fb39c6d8e10ae7087c0888a12320605d9
Ernesto Cantero <ecantero@student.neumont.edu>
2019-02-28 14:34:12 -0700

    Changes to the main.java
    the main menu is complete

--- ArcadeProject/src/application/Main.java
+++ ArcadeProject/src/application/Main.java
@@ -1,25 +1,89 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import controller.Gameboard;
+import controller.UNO;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
+
+	Button playUNO = new Button("Play UNO");
+	Button playCheckers = new Button("Play Checkers");
+	StackPane background = new StackPane();
+	Button submit = new Button("Submit");
+	UNO one = new UNO();
+	Gameboard board = new Gameboard();
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,500,500);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+			playUNO.setOnAction(new EventHandler<ActionEvent>() {
+
+				@Override
+				public void handle(ActionEvent event) {
+					try {
+						// insert UNO stage here
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+
+				}
+			});
+
+			playCheckers.setOnAction(new EventHandler<ActionEvent>() {
+
+				@Override
+				public void handle(ActionEvent event) {
+					try {
+						board.start(primaryStage);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			});
+
+			String url = "file:Sprites/ArcadeLogo.jpg";
+			Image backGround = new Image(url);
+			// ImageView view= new ImageView(url);
+			BackgroundImage back = new BackgroundImage(backGround, null, null, null,
+					new BackgroundSize(800, 800, false, false, true, true));
+
+			background.setBackground(new Background(back));
+
+			Scene scene = new Scene(background, 800, 800);
+
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Arcade: UNO or Checkers");
+			background.setPadding(new Insets(20, 80, 20, 80));
+			playUNO.setTranslateX(-50);
+			playUNO.setTranslateY(-100);
+			playCheckers.setTranslateX(100);
+			playCheckers.setTranslateY(200);
+			background.getChildren().add(playUNO);
+			background.getChildren().add(playCheckers);
+
+			primaryStage.setResizable(false);
+
 			primaryStage.show();
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
