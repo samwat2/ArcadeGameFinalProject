6f88450246e200a94546db118f690593f2259d56
Ernesto Cantero <ecantero@student.neumont.edu>
2019-03-06 13:36:18 -0700

    Made them all caps to match the enums of the cards

--- ArcadeProject/src/controller/UNO.java
+++ ArcadeProject/src/controller/UNO.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.util.ArrayList;
 import java.util.Random;
+import enums.CardColor;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -31,18 +32,19 @@ public class UNO extends Application {
 	public static Deck deck;// all 112 cards.
 //	public static Card[] discardPile; // discard pile
 	public static ArrayList<Card> discardPile;
-	public Button endTurn = new Button();
-	private ButtonBase draw = new Button();
+	private Button endTurn = new Button("End Turn");
+	private Button draw = new Button("Draw");
 	private StackPane background = new StackPane();
-	
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			draw .setOnAction(new EventHandler<ActionEvent>() {
+			draw.setOnAction(new EventHandler<ActionEvent>() {
 
 				@Override
 				public void handle(ActionEvent event) {
-					// This will have the player to draw a card form the draw pile, can only do once per turn
+					// This will have the player to draw a card form the draw pile, can only do once
+					// per turn
 
 				}
 			});
@@ -54,20 +56,28 @@ public class UNO extends Application {
 
 				}
 			});
-			deck = new Deck();
 			String url = "file:Sprites/red background.jpg";
 			Image backGround = new Image(url);
-			// ImageView view= new ImageView(url);
 			BackgroundImage back = new BackgroundImage(backGround, null, null, null,
 					new BackgroundSize(800, 800, false, false, true, true));
 
+			Image image = new Image("file:Sprites/" + card.getCardColor() + " " + card.getFaceValue() + ".png", 100, 100,
+					false, false);
+
 			background.setBackground(new Background(back));
 
-			Scene scene = new Scene(background, 1050, 800);
+			Scene scene = new Scene(background, 1000, 800);
 
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Arcade: UNO");
 			background.setPadding(new Insets(20, 80, 20, 80));
+			draw.setTranslateX(100);
+			draw.setTranslateY(150);
+			endTurn.setTranslateX(100);
+			endTurn.setTranslateY(200);
+			background.getChildren().add(draw);
+			background.getChildren().add(endTurn);
+			
 
 			primaryStage.setResizable(false);
 
@@ -88,16 +98,16 @@ public class UNO extends Application {
 	public static void createPlayers() {
 		int input2, count = 0;
 		boolean correct = false;
-		//text-field for amount of players
+		// text-field for amount of players
 		final TextField amountOfPlayers = new TextField();
-		//text-field text
+		// text-field text
 		amountOfPlayers.setText("How many players will be joining?");
-		//gathered input
+		// gathered input
 		String input = amountOfPlayers.getText();
-		//parsed input
+		// parsed input
 		int numOfPlayers = Integer.parseInt(input);
 		do {
-			//player's info per player
+			// player's info per player
 			final TextField name = new TextField();
 			name.setText("Enter your name.");
 			PasswordField key = new PasswordField();
@@ -117,18 +127,18 @@ public class UNO extends Application {
 		// shuffle deck
 		deck.shuffleDeck();
 //		initial
-		discardPile.add(deck.getCards()[0]); 
+		discardPile.add(deck.getCards()[0]);
 		// checks key for player
 		final PasswordField key = new PasswordField();
 		key.setText("Please enter your key");
 		int input = Integer.parseInt(key.getText());
-		if(input == player.getKey()) {
-		 drawHand(player);
-		}else {
+		if (input == player.getKey()) {
+			drawHand(player);
+		} else {
 //		 throw and error and key again.
-				//do-while
+			// do-while
 		}
-		//proceed to game
+		// proceed to game
 		play();
 		/*
 		 * 1) player one enter's pin draws first player must lay a matching color down.
@@ -139,39 +149,39 @@ public class UNO extends Application {
 	}
 
 	public static void checkHand() {
-		//TODO
-		
+		// TODO
+
 	}
 
 	public static void play() {
-		//TODO
+		// TODO
 		// what options the player had to play
 		for (int i = 0; i < 7; i++) {
-			//if the card-color has the same color
-			if(player.getHand()[i].getCardColor() == currentCard().getCardColor()) {
-				//has the option of laying down the card
+			// if the card-color has the same color
+			if (player.getHand()[i].getCardColor() == currentCard().getCardColor()) {
+				// has the option of laying down the card
 				discardPile.add(player.getHand()[i]);
-					//player must draw new card
+				// player must draw new card
 				draw();
 			}
-			//if the face value is the same integer
+			// if the face value is the same integer
 			if (player.getHand()[i].getFaceValue() == currentCard().getFaceValue()) {
-				//has the option of laying down the card
+				// has the option of laying down the card
 				discardPile.add(player.getHand()[i]);
-					//player must draw new card
-				draw();//has the option of laying down the card
+				// player must draw new card
+				draw();// has the option of laying down the card
 			}
 
 		}
 	}
-	
+
 	public static void draw() {
 		player.addCard(deck.getCards()[0]);
 	}
 
 	public static Card currentCard() {
 		// gets the current card in play for the discardPile
-		card = discardPile.get(discardPile.size()-1);
+		card = discardPile.get(discardPile.size() - 1);
 		return card;
 	}
 
@@ -183,7 +193,7 @@ public class UNO extends Application {
 	}
 
 	public static void drawHand(Player player) {
-		//THROWS ERRORS ON 128
+		// THROWS ERRORS ON 128
 		// every player initially gets a hand of seven cards
 		Card[] newHand = new Card[7];
 		for (int q = 0; q < 7; q++) {
