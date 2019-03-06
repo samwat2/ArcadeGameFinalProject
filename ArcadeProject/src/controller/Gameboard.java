5d14457e9c4bba8ff592dba98590f4dbd6ff53ed
piggys21 <jordanisem@gmail.com>
2019-03-06 14:18:44 -0700

    clean up

--- ArcadeProject/src/controller/Gameboard.java
+++ ArcadeProject/src/controller/Gameboard.java
@@ -83,7 +83,7 @@ public class Gameboard extends Application {
 			int col = (int) (e.getX() / 100);
 			
 			if (tempPiece == null) {
-				// System.out.printf("%s %s\n", (int)(e.getX() / 100), (int)(e.getY() / 100));
+			
 				resetBackground(root, size);
 				if (pieces[row][col] != null) {
 					if (pieces[row][col].color.equals(turn))
@@ -103,7 +103,6 @@ public class Gameboard extends Application {
 							(int) (e.getY() / 100));
 
 					mattePane.setStyle("-fx-background-color: yellow;");
-					// mattePane.setBackground(background);
 					tempPiece = pieces[row][col];
 				}
 			} else {
@@ -217,7 +216,7 @@ public class Gameboard extends Application {
 					attack = true;
 					if(pieces[7][0] ==pieces[7][0]) {
 						tempPiece.setKing(true);
-						//String pieceImage = pieces[7][7].getColor();
+						
 
 						Image image = new Image("file:Sprites/" + "Red King" + ".png", 100, 100, false, false);
 						root.add(new ImageView(image), 7, 7);
@@ -232,13 +231,7 @@ public class Gameboard extends Application {
 		}
 		
 	}
-//	public void KingMaker() {
-//		if (tempPiece.getColor() == "Red Piece") {
-//			if (pieces[move.getRow()][move.getCol()] != null
-//					&& pieces[move.getRow() - 1][move.getCol() + 1] == null) {
-//				tempPiece.getMoves().add(new Move(move.getRow() - 1, move.getCol() + 1));
-//				attack = true;
-//			}
+
 	
 		
 	}
